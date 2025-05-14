f6f094b6137fb269f7917ab1d9716bfba5754444
jeansi07 <jeansi123@live.com>
2025-05-14 16:01:07 -0500

    feat: post documentation in swagger

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ load_dotenv()
 client = genai.Client(api_key=os.getenv("API_KEY"))
 app = FastAPI()
 
-@app.post("/message", response_model=MessageResponse)
+@app.post("/message", response_model=MessageResponse, summary="Invocar el modelo Gemini para procesamiento de texto",
+    description="""Este endpoint expone una interfaz POST para consumir los servicios del modelo de lenguaje **Gemini**, 
+    enviando una solicitud con un prompt de entrada.""")
+
 def read_item(message: MessageRequest):
     response = client.models.generate_content(
         model="gemini-2.0-flash", contents=message.content
